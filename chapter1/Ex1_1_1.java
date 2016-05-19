17dfa2c3966db36238f24b283e97dd025e51ca7a
yunqixu <wind891209@126.com>
2016-05-19 14:28:29 +0800

    First Commit

---
+++ chapter1/Ex1_1_1.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class Ex1_1_1 {
+	public static void main(String[] args){
+		int a = (0 + 15) / 2;
+		double b = 2.0e-6 * 100000000.1;
+		boolean c = true && false || true && true;
+		System.out.println(a + " " + b + " " + c);
+	}
+}
