4309e848667d63d2c66eb31051c37cd9f52be22f
yunqixu <wind891209@126.com>
2016-07-18 18:56:05 +0800

    first commit

---
+++ chapter1/Ex1_1_3.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1_3 {
+	public static void main(String[] args){
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		if (a == b && b == c) StdOut.print("equal");
+		else StdOut.print("not equal");
+	}
+}
